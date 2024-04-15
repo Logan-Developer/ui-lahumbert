341b94d97d391504f5648b358ab874a60dced67c
Logan-Developer <logan2.humbert@gmail.com>
2024-04-15 13:20:48 -0600

    FinalProject: remove unused import

--- FinalProject/note_taking_app/lib/widgets/my_quill_toolbar.dart
+++ FinalProject/note_taking_app/lib/widgets/my_quill_toolbar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quill/extensions.dart' show isWeb;
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_quill_extensions/flutter_quill_extensions.dart';
 
